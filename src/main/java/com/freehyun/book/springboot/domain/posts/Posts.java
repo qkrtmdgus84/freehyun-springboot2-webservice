1722f99ad296f7e7aa0e7e04f9e4425481e298fc
84sh <84sh@naver.com>
2020-03-10 15:12:42 +0900

    BaseTimeEntity 등록

--- src/main/java/com/freehyun/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/freehyun/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.freehyun.book.springboot.domain.posts;
 
+import com.freehyun.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter //클래스 내 모든 필드의 Getter 메소드를 자동생성
 @NoArgsConstructor //기본 생성자 자동 추가, public Posts() {}와 같은 효과
 @Entity //테이블과 링크될 클래스, 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭, ex)SalesManager.java -> sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id //해당 테이블의 PK 필드를 나타낸다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK 의 생성 규칙을 나타낸다. 스프링 부트 2.0 에서는 Generation Type.IDENTITY 옵션을 추가해야만 auto_increment 가 된다.
