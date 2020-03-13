d4f8f52c21c75902bf86a7e5b6b611e559cb7e71
84sh <84sh@naver.com>
2020-03-13 10:11:51 +0900

    스프링 시큐리트 구글 로그인

---
+++ src/main/java/com/freehyun/book/springboot/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.freehyun.book.springboot.domain.user;
+
+import com.freehyun.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) //JPA 로 데이터베이스로 저장할 때 Enum 값을 어떤 형태로 저장할지를 결정, 기본적으로 int 로 된 숫자가 저장, 숫자로 저장되면 데이터베이스로 확인할 때 그 값이 무슨 코드를 의미하는지 알 수가 없다. 그래서 문자열 (EnumType.STRING)로 저장될 수 있도록 선언.
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
--- src/main/java/com/freehyun/book/springboot/web/IndexController.java
+++ src/main/java/com/freehyun/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.freehyun.book.springboot.web;
 
+import com.freehyun.book.springboot.config.auth.dto.SessionUser;
 import com.freehyun.book.springboot.service.PostsService;
 import com.freehyun.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { //Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장, 여기서는 postsService.findAllDesc()로 가져온 결과를 posts 로 index.mustache 에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장. 즉, 로그인 성공 시 httpSession.getAttribute("user") 에서 값을 가져올 수 있다.
+
+        if (user != null) { //세션에 저장된 값이 있을 때만 model 에 userName 으로 등록. 세션에 저장된 값이 없으면 model 엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
