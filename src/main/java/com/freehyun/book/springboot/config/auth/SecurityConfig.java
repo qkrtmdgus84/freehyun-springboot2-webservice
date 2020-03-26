17bee69fca78e70ca5333296650205893ec55f3f
84sh <84sh@naver.com>
2020-03-26 17:45:13 +0900

    profile API 추가, profile 테스트 코드 추가

--- src/main/java/com/freehyun/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/freehyun/book/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션들을 disable 한다.
         .and()
                 .authorizeRequests() //URL 별 권한 관리를 설정하는 옵션의 시작점, authorizeRequests 가 선언되어야만 antMatchers 옵션을 사용할 수 있다.
-                .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //권한 관리 대상을 지정하는 옵션, URL, HTTP 메소드별로 관리가 가능, "/"등 지정된 URL 들은 permitAll() 옵션을 통해 전체 열람 권한을 준다, "/api/v1/**" 주소를 가진 API 는 USER 권한을 가진 사람만 가능.
                 .anyRequest().authenticated() //설정된 값들 이외 나머지 URL 들을 나타낸다. 여기서는 authenticated()을 추가하여 나머지 URL 들은 모두 인증된 사용자들에게만 허용, 인증된 사용자 즉, 로그인한 사용자들을 이야기한다.
         .and()
