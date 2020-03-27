285ab68fb222ecf1183842473ec5e3c416a38cab
84sh <84sh@naver.com>
2020-03-27 13:48:25 +0900

    nginx 설정 + profile api 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,16 +9,16 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f1 freehyun-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl freehyun-springboot2-webservice | grep jar | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
